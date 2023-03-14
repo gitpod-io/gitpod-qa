976f06955034a23a8b62c90c563656daf9912b97
GHOST <ghostdevbusiness@gmail.com>
2023-03-14 00:59:34 +0000

    refactor: add a config

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const config: Config = {
+    question: 'What is windows?',
+    searchIndex: 'wikipedia'
+}
+
+export interface Config {
+    question: string;
+    searchIndex: 'wikipedia'
+}
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import { loadQAStuffChain } from 'langchain/chains';
 import { HNSWLib } from 'langchain/vectorstores';
 import { Document } from 'langchain/document';
 import { OpenAI } from 'langchain/llms';
+import { config } from './config'; 
 import { existsSync } from 'fs';
 import { join } from 'desm';
 
-const SEARCH_PATH = join(import.meta.url, '../search-cache');
+const SEARCH_PATH = join(import.meta.url, `../search-index/${config.searchIndex}`);
 
 async function getDocuments() {
     const documents: Document[] = [];
@@ -63,8 +64,6 @@ async function generateSearchIndex(documents: Document[]) {
 
 let search: HNSWLib;
 
-console.log('Loading documents')
-
 if (existsSync(SEARCH_PATH)) {
     console.log('Using existing search index');
     search = await HNSWLib.load(SEARCH_PATH, new OpenAIEmbeddings());
@@ -79,11 +78,13 @@ const chain = loadQAStuffChain(new OpenAI({ temperature: 0 }));
 
 console.log('Loaded');
 
-const question = 'What is Windows?'
+const documents = await search.similaritySearch(config.question, 4)
 
 const result = await chain.call({
-    input_documents: await search.similaritySearch(question, 4),
-    question,
+    input_documents: documents,
+    question: config.question,
 });
 
 const answer = result.text;
+
+console.log({ answer })
