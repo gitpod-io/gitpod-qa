54e4b0cd6b3ead23b363121c7c0437bc10cabb5e
GHOST <ghostdevbusiness@gmail.com>
2023-03-15 00:47:13 +0000

    testing

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,14 @@
+import { readFileSync, writeFile, writeFileSync } from 'fs';
 import { getDocuments } from './documents';
+import { markdownToText } from './marked';
+import { marked } from 'marked';
+import { join } from 'desm';
+
+const TEMP_PATH = join(import.meta.url, '../temp');
 
 const documents = await getDocuments();
 console.log(documents.length);
+
+const str = readFileSync(`${TEMP_PATH}/in.md`, 'utf-8');
+
+writeFileSync(`${TEMP_PATH}/out.txt`, markdownToText(str), 'utf-8');
