b95f18969b889375ccfc137653b1ab65aa162bd9
GHOST <ghostdevbusiness@gmail.com>
2023-04-15 15:57:02 +0000

    feat: switch to a button

---
+++ packages/discord/src/buttons/question.ts
@@ -0,0 +1,37 @@
+import { generate_answer } from '../lib/qa';
+import { ChannelType } from 'discord.js';
+import { button } from 'jellycommands';
+
+export default button({
+    id: 'question-qa',
+
+    defer: true,
+
+    async run({ interaction, client, props }) {
+        const channel = await client.channels.fetch(interaction.channelId);
+
+        if (!channel)
+            return interaction.followUp({
+                content: 'Unable to find the channel you are in',
+            });
+
+        if (channel.type != ChannelType.PublicThread)
+            return interaction.followUp({
+                content: 'Not in a thread',
+            });
+
+        const startMessage = await channel.fetchStarterMessage();
+
+        if (!startMessage)
+            return interaction.followUp({
+                content: 'Unable to fetch question',
+            });
+
+        const question = `${channel.name.trim()}\n${startMessage.content.trimStart()}`;
+        const answer = await generate_answer(question, props.search);
+
+        return interaction.followUp({
+            content: answer,
+        });
+    },
+});
---
+++ packages/discord/src/lib/qa.ts
@@ -0,0 +1,24 @@
+import { create_error_response, create_response } from './response';
+import type { QASearcher } from '@gitpod/docs-qa';
+
+export async function generate_answer(question: string, search: QASearcher) {
+    const result = await search(question);
+
+    const sources: string[] = Array.isArray(result?.sources)
+        ? result.sources
+        : [];
+
+    const answer = typeof result?.answer == 'string' ? result.answer : null;
+
+    if (!answer || sources.length == 0)
+        return 'There was an error generating an answer';
+
+    // The prompt template instructs it to return "I don't know" if it doesn't know, so we should exit
+    if (answer == "I don't know.") return create_error_response(question);
+
+    return create_response({
+        question,
+        sources,
+        answer,
+    });
+}
