c41ec86c50ced30d3e7fbf10293232e9dd87170e
GHOST <ghostdevbusiness@gmail.com>
2023-05-02 23:29:03 +0000

    fix: move /index logic and clean up

--- packages/discord/src/lib/qa.ts
+++ packages/discord/src/lib/qa.ts
@@ -2,17 +2,11 @@ import { create_error_response, create_response } from './response';
 import type { QASearcher } from '@gitpod/docs-qa';
 
 export async function generate_answer(question: string, search: QASearcher) {
-    const result = await search(question);
+    const { answer, sources } = await search(question);
 
-    const sources: string[] = Array.isArray(result?.sources)
-        ? // TODO remove the map next time the search index is rebuilt
-          result.sources.map((source) => source.replace(/\/index$/gm, '/'))
-        : [];
-
-    const answer = typeof result?.answer == 'string' ? result.answer : null;
-
-    if (!answer || sources.length == 0)
+    if (sources.length == 0) {
         return 'There was an error generating an answer';
+    }
 
     // The prompt template instructs it to return "I don't know" if it doesn't know, so we should exit
     if (answer == "I don't know.") return create_error_response(question);
